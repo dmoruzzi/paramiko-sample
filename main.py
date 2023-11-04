aaeabafdad2f6d43bf912b519c16f9299fca5f12
David Moruzzi <dmoruzzi@users.noreply.github.com>
2023-11-04 02:20:49 -0400

    Improved logging

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import paramiko
 import yaml
+import logging
+
+# Configure logging
+log_format = "%(asctime)s [%(levelname)s]: %(message)s"
+logging.basicConfig(filename='ssh_operations.log', level=logging.DEBUG, format=log_format)  # Set the log level to DEBUG
 
 def connect(host, username, key_file=None, password=None, port=22):
+    logging.debug(f"Connecting to {host}...")
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -11,25 +17,34 @@ def connect(host, username, key_file=None, password=None, port=22):
             ssh.connect(host, port, username, pkey=key)
         else:
             ssh.connect(host, port, username)
+        logging.debug(f"Connected to {host}.")
         return ssh
     except Exception as e:
-        print(f"Connection error to {host}: {e}")
+        error_msg = f"Connection error to {host}: {e}"
+        logging.error(error_msg)  # Log the error
         return None
 
 def execute_with_input(ssh, cmd, input_responses=None):
     if ssh:
+        logging.debug(f"Executing command: {cmd}")
         stdin, stdout, stderr = ssh.exec_command(cmd)
         if input_responses:
             for response in input_responses:
                 stdin.write(response + "\n")
                 stdin.flush()
-        return stdout.read().decode()
+        output = stdout.read().decode()
+        logging.debug(f"Command '{cmd}' executed with output: {output}")  # Debug log
+        return output
     else:
         return None
 
 def disconnect(ssh):
     if ssh:
+        host = ssh.get_transport().getpeername()[0]
+        logging.debug(f"Disconnecting from {host}...")
         ssh.close()
+        logging.debug(f"Disconnected from {host}.")
+        return None  # Reset the ssh_client
 
 if __name__ == '__main__':
     with open('config.yaml', 'r') as config_file:
@@ -67,5 +82,6 @@ if __name__ == '__main__':
                 for command, definition in command_definitions.items():
                     input_responses = definition['input_responses']
                     output = execute_with_input(ssh_client, command, input_responses)
-                    print(f"{server_info['host']}: {command} -> {output}")
-                disconnect(ssh_client)
+                    log_message = f"{server_info['host']}: {command} -> {output}"
+                    logging.info(log_message)
+                ssh_client = disconnect(ssh_client)
